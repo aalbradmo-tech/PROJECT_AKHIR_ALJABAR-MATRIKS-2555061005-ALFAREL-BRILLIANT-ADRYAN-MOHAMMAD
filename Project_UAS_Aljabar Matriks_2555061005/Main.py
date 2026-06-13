3d1b24e97b117116eb05f22eeea777fa74ba9f1b
aalbradmo-tech <aalbradmo@gmail.com>
2026-06-13 23:57:07 +0700

    Create Main.py

---
+++ Project_UAS_Aljabar Matriks_2555061005/Main.py
@@ -0,0 +1,25 @@
+A = [[1, 2, 4],
+     [3, 4, 5],
+     [6, 7, 8]]
+
+B = [[2, 5, 5],
+     [1, 0, 6],
+     [0, 0, 5]]
+
+print("Matriks A:")
+print(A)
+
+print("\nMatriks B:")
+print(B)
+
+print("\nPenjumlahan Matriks A + B:")
+print(tambah_matriks(A, B))
+
+print("\nPengurangan Matriks A - B:")
+print(kurang_matriks(A, B))
+
+print("\nPerkalian Matriks A x B:")
+print(kali_matriks(A, B))
+
+print("\nTranspose Matriks A:")
+print(transpose_matriks(A))
