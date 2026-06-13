576bf54d25c5dcc2df64d27431f2eceaac549dfb
aalbradmo-tech <aalbradmo@gmail.com>
2026-06-14 00:26:16 +0700

    Update Main.py

--- Project_UAS_Aljabar Matriks_2555061005/Main.py
+++ Project_UAS_Aljabar Matriks_2555061005/Main.py
@@ -1,3 +1,4 @@
+import Alfarel005 as n
 
 A = [[1, 2, 3],
      [4, 5, 6],
@@ -7,14 +8,20 @@ B = [[2, 5, 5],
      [0, 6, 1],
      [0, 0, 5]]
 
+D = [[4, 3],
+     [2, 1]]
+
 print("\nPenjumlahan Matriks A + B:")
-print(tambah_matriks(A, B))
+print(n.tambah_matriks(A, B))
 
 print("\nPengurangan Matriks A - B:")
-print(kurang_matriks(A, B))
+print(n.kurang_matriks(A, B))
 
 print("\nPerkalian Matriks A x B:")
-print(kali_matriks(A, B))
+print(n.kali_matriks(A, B))
 
 print("\nTranspose Matriks A:")
-print(transpose_matriks(A))
+print(n.transpose_matriks(A))
+
+print("\nDeterminan Matriks D:")
+print(n.determinan_2x2(D))
