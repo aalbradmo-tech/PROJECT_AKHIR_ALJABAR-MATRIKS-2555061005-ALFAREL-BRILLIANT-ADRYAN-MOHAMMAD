8f1da70619a30d1f1984529506195d7ad5eccf76
aalbradmo-tech <aalbradmo@gmail.com>
2026-06-14 00:11:28 +0700

    Update Main.py

--- Project_UAS_Aljabar Matriks_2555061005/Main.py
+++ Project_UAS_Aljabar Matriks_2555061005/Main.py
@@ -1,16 +1,11 @@
+
 A = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
 
 B = [[2, 5, 5],
-     [1, 0, 6],
+     [0, 6, 1],
      [0, 0, 5]]
-
-print("Matriks A:")
-print(A)
-
-print("\nMatriks B:")
-print(B)
 
 print("\nPenjumlahan Matriks A + B:")
 print(tambah_matriks(A, B))
