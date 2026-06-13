926531e23145ebaf83895201732bfae94b59a9ed
aalbradmo-tech <aalbradmo@gmail.com>
2026-06-13 23:56:09 +0700

    Update Alfarel005.py

--- Project_UAS_Aljabar Matriks_2555061005/Alfarel005.py
+++ Project_UAS_Aljabar Matriks_2555061005/Alfarel005.py
@@ -1,47 +1,47 @@
-def tambah_matriks(A, B):
+def tambah_matriks(m1, m2):
     hasil = []
-    for i in range(len(A)):
+    for i in range(len(m1)):
         baris = []
-        for j in range(len(A[0])):
-            baris.append(A[i][j] + B[i][j])
+        for j in range(len(m1[0])):
+            baris.append(m1[i][j] + m2[i][j])
         hasil.append(baris)
     return hasil
 
 
-def kurang_matriks(A, B):
+def kurang_matriks(m1, m2):
     hasil = []
-    for i in range(len(A)):
+    for i in range(len(m1)):
         baris = []
-        for j in range(len(A[0])):
-            baris.append(A[i][j] - B[i][j])
+        for j in range(len(m1[0])):
+            baris.append(m1[i][j] - m2[i][j])
         hasil.append(baris)
     return hasil
 
 
-def kali_matriks(A, B):
+def kali_matriks(m1, m2):
     hasil = []
-    for i in range(len(A)):
+    for i in range(len(m1)):
         baris = []
-        for j in range(len(B[0])):
-            total = 0
-            for k in range(len(B)):
-                total += A[i][k] * B[k][j]
-            baris.append(total)
+        for j in range(len(m2[0])):
+            jumlah = 0
+            for k in range(len(m2)):
+                jumlah += m1[i][k] * m2[k][j]
+            baris.append(jumlah)
         hasil.append(baris)
     return hasil
 
 
-def transpose_matriks(A):
+def transpose_matriks(m):
     hasil = []
-    for j in range(len(A[0])):
+    for j in range(len(m[0])):
         baris = []
-        for i in range(len(A)):
-            baris.append(A[i][j])
+        for i in range(len(m)):
+            baris.append(m[i][j])
         hasil.append(baris)
     return hasil
 
 
-def determinan_2x2(A):
-    if len(A) != 2 or len(A[0]) != 2:
-        return 
-    return A[0][0] * A[1][1] - A[0][1] * A[1][0]
+def determinan_2x2(m):
+    if len(m) != 2 or len(m[0]) != 2:
+        return None
+    return m[0][0] * m[1][1] - m[0][1] * m[1][0]
